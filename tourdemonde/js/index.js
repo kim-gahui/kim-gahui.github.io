5c9c4957575c3abfbe2be9c3d72cd8fcb8a3995c
kim-gahui <gahee4@gmail.com>
2018-03-14 15:17:56 +0900

    edit file

--- tourdemonde/js/index.js
+++ tourdemonde/js/index.js
@@ -2,6 +2,7 @@ $(function(){
   $('a').on('click', function(e){
     e.preventDefault();
   })
+
   var swiper = new Swiper('.swiper-container', {
     navigation: {
       prevEl: '.swiper-button-prev',
@@ -23,12 +24,12 @@ $(function(){
   $('.slide .prev').on('click', function(){
     i--;
     if(i<0){i=length-5};
-    $('.photo-slide').animate({'left':-(i*200)},800)
+    $('.photo-slide').animate({'left':-(i*200)},800);
   })
   $('.slide .next').on('click', function(){
     i++;
     if(i>length-5){i=0};
-    $('.photo-slide').animate({'left':-(i*200)},800)
+    $('.photo-slide').animate({'left':-(i*200)},800);
   })
 
   $(window).scroll(function(){
@@ -43,12 +44,12 @@ $(function(){
       $('aside').css({
         position:'absolute',
         bottom:850,
-        top:'initial'
+        top:'auto'
       });
     }else{
       $('aside').css({
         position:'fixed',
-        bottom:'initial',
+        bottom:'auto',
         top:160
       });
     }
