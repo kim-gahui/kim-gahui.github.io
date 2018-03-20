1df8d08bb64477f268f2cf72779e1e9c5366490e
kim-gahui <gahee4@gmail.com>
2018-03-20 16:02:21 +0900

    edit

--- js/portfolio.js
+++ js/portfolio.js
@@ -11,6 +11,7 @@ $(function(){
   )
   wow.init();
 
+  // 네비게이션 삽입하지 않기로 함. 관련한 스크립트 모두 주석처리.
   $(window).scroll(function(){
     var home=$('#home').offset().top;
     var about=$('#about').offset().top-300;
@@ -28,14 +29,14 @@ $(function(){
     }else if(winTop>=contact){
       index=3;
     }
-    $('nav li a').removeClass('active');
-    $('nav li').eq(index).find('a').addClass('active');
-
-    if(winTop>=home && winTop<(about+300)){
-      $('.gnb-nav').css('display','none');
-    }else{
-      $('.gnb-nav').css('display','block');
-    }
+    // $('nav li a').removeClass('active');
+    // $('nav li').eq(index).find('a').addClass('active');
+    //
+    // if(winTop>=home && winTop<(about+300)){
+    //   $('.gnb-nav').css('display','none');
+    // }else{
+    //   $('.gnb-nav').css('display','block');
+    // }
 
     if(winTop>=(portfolio+100) && winTop<(contact-200)){
       $('.swiper-button-next, .swiper-button-prev').fadeIn();
@@ -44,21 +45,21 @@ $(function(){
     }
   }).scroll();
 
-  $('.btn-toggle').on('click', function(){
-    $('nav').slideToggle();
-  })
-
-  $('nav a').on('click', function(e){
-    e.preventDefault();
-    if(this.hash !== ""){
-      var hash=this.hash;
-      $('html,body').animate({
-        scrollTop:$(hash).offset().top
-      },800, function(){
-        window.location.href=hash;
-      });
-    }
-  })
+  // $('.btn-toggle').on('click', function(){
+  //   $('nav').slideToggle();
+  // })
+  //
+  // $('nav a').on('click', function(e){
+  //   e.preventDefault();
+  //   if(this.hash !== ""){
+  //     var hash=this.hash;
+  //     $('html,body').animate({
+  //       scrollTop:$(hash).offset().top
+  //     },800, function(){
+  //       window.location.href=hash;
+  //     });
+  //   }
+  // })
 
   $('.scroll a').on('mouseenter', function(){
     $(this).text('or click me');
@@ -68,17 +69,17 @@ $(function(){
 
   $('.scroll a').on('click',function(e){
     e.preventDefault();
-    $('nav').css('display','none');
+    // $('nav').css('display','none');
     $('html,body').animate({
       scrollTop:$('#about').offset().top
     },800);
   })
 
-  $('#contact input, #contact textarea').on('focusin', function(){
-    $(this).prev().css('color', '#e29898');
-  }).on('focusout', function(){
-    $(this).prev().css('color', '#232323');
-  })
+  // $('#contact input, #contact textarea').on('focusin', function(){
+  //   $(this).prev().css('color', '#e29898');
+  // }).on('focusout', function(){
+  //   $(this).prev().css('color', '#232323');
+  // })
 
   var swiper = new Swiper('.swiper-container', {
     navigation:{
